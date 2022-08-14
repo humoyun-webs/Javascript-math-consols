fbbc9c3df72ae2a03dd9d5f63b0aa226151b92b9
humoyun-webs <humoyuneshpolatov5@gmail.com>
2022-08-14 02:29:26 -0800

    Javascript first math consols

---
+++ script.js
@@ -0,0 +1,239 @@
+// #1
+// {
+// let a =+ prompt("A son kiriting soningizni  manfiy yoki musbat ekanligini bilib olasiz")
+
+// if(a<=1){
+// console.log("Manfiy son")
+// }else{
+//     console.log("Musbat son son")
+// }
+// }
+
+
+
+
+// #2 #3
+// {
+//     let a =+ prompt("B son kiriting soningizni  juft yoki musbat ekanligini bilib olasiz")
+
+//     if(a % 2 == 0){
+//         console.log("Juft son")
+//     }else{
+//         console.log("Toq son")
+//     }
+// }
+
+
+
+
+
+// #4
+// {
+//    let son = 211;
+// let onlik = son/10;
+// let qoldiq = son%10/10;
+// let onlik1 = onlik - qoldiq;
+// let birlik = son%10;
+// console.log(onlik1 + " onlik " + birlik + " birlik");
+// }
+
+
+
+
+
+// #5
+// {
+ // let a= 37
+ // let b= 35
+ // let c= 12
+
+
+ // if((a > b > c)){
+ // console.log("t")
+ // }else{
+ // console.log("f")
+ // }
+// }
+
+
+
+
+// #6
+// let a = 1
+// let b = 3
+// let c = 6
+
+// if (a < b || b > c) {
+//     console.log("t");
+// }else{
+//     console.log("f");
+// }
+
+
+
+// #7
+// {
+//     let a =+ prompt(" son kiriting ") 
+
+//     if(a % 2 == 0){
+//                 console.log("Toq son emas")
+//             }else{
+//                 console.log("Toq son")
+//             }
+
+// }
+
+
+
+
+
+// #8 #9
+// {
+//    let a =+ prompt("1-sonni kiring")
+// let b =+ prompt("2-sonni kiring")
+// let c =+ prompt("3-sonni kiring")
+
+// if((a<1) || (b<1) || (c<1)){
+// console.log("Ichida 1 ta son yoki undan koproq bolgan son manfiy")
+// }else{
+// console.log("Sonlarning barchasi musbat")
+// }
+// }
+
+
+
+
+
+// #10
+// {
+// let a =+prompt("Hafta kunini yozing faqat 1-14 gacha !")
+// if (a == 1) {
+//     alert("Dushanba")
+// }else if (a == 2) {
+//     alert("Seshanba")
+// }else if (a == 3){
+//     alert("Chorshanba")
+// }else if (a == 4){
+//     alert("Payshanba")
+// }else if (a == 5) {
+//     alert("Juma")
+// }else if (a == 6) {
+//     alert("Shanba")
+// }else if (a == 7) {
+//     alert("Yakshanba")
+// }if (a == 8) {
+//     alert("Dushanba")
+// }else if (a == 9) {
+//     alert("Seshanba")
+// }else if (a == 10){
+//     alert("Chorshanba")
+// }else if (a == 11){
+//     alert("Payshanba")
+// }else if (a == 12) {
+//     alert("Juma")
+// }else if (a == 13) {
+//     alert("Shanba")
+// }else if (a == 14) {
+//     alert("Yakshanba")
+// }
+// console.log(a);
+// }
+
+
+
+
+
+// #11
+// let companyName =+ prompt("Telefon raqamingizni kodini kiring")
+
+// if(companyName==97){
+//     console.log("Telefoningiz ums tarifida")
+
+// }else if(companyName==98 || companyName==99){
+//     console.log("Telefoningiz Uzmobile  tarifida")
+
+// }else if(companyName==90 || companyName==91){
+//     console.log("Telefoningiz beline tarifida tarifida")
+
+// }
+
+
+
+
+
+// #12
+// {
+//     let son =+ prompt("son kiriting")  
+
+
+//     if(son<1){
+//         son= son-1
+//         console.log('soningiz 1 ga ayirildi;' + son)
+        
+//     }else{
+//         son= son+1
+//         console.log('soningiz 1 ga qoshildi:' + son)
+//     }
+// }
+
+
+
+
+// #13
+    // {
+    //         let son =+ prompt("son kiriting")  
+        
+        
+    //         if(son<1){
+    //             son= son-2
+    //             console.log('soningiz 2 ga ayirildi;' + son)
+                
+    //         }else{
+    //             son= son+3
+    //             console.log('soningiz 3 ga qoshildi:' + son)
+    //         }
+    //     } 
+
+
+
+
+    // #14
+    // {
+    //     let a =+ prompt ("1-sonni kiritng")
+    //     let b =+ prompt ("2-sonni kiritng")
+
+    //     if(a<b){
+    //         console.log(b + " son katta")
+        
+    //     }else{
+    //         console.log(a + " son katta")
+    //     }
+    // }
+
+
+
+
+
+
+    // #15
+//     {
+// let a = +prompt("1-son kiriting")
+// let b = +prompt("2-sonni kiriting")
+// let c = +prompt("3-sonni kiriting")
+// let max = Math.max(a , b , c);
+// console.log(max + " katta son")
+//     }
+
+
+
+
+// #16
+// {
+    
+//         let a = +prompt("1-son kiriting")
+//         let b = +prompt("2-sonni kiriting")
+//         let c = +prompt("3-sonni kiriting")
+//         let min = Math.min(a , b , c);
+//         console.log(min + " kichik son")
+//     }
+          
